6cd3f9d9fb55774c0e051a885258031efd69de33
Enrique Gomez <enrique.gomez@apiquality.io>
2023-08-11 08:52:25 +0200

    changes to security

---
+++ src/test/java/org/sonar/samples/openapi/checks/security/OAR082BinaryOrByteFormatCheckTest.java
@@ -0,0 +1,37 @@
+package org.sonar.samples.openapi.checks.security;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sonar.api.rule.Severity;
+import org.sonar.api.rules.RuleType;
+import org.sonar.api.server.rule.RuleParamType;
+import org.sonar.samples.openapi.BaseCheckTest;
+
+public class OAR082BinaryOrByteFormatCheckTest extends BaseCheckTest {
+
+    @Before
+    public void init() {
+        ruleName = "OAR082";
+        check = new OAR082BinaryOrByteFormatCheck();
+        v2Path = getV2Path("security");
+        v3Path = getV3Path("security");
+    }
+    @Test
+    public void verifyvalidV2() {
+        verifyV2("valid-format");
+    }
+    @Test
+    public void verifyvalidV3() {
+        verifyV3("valid-format");
+    }
+
+    @Override
+    public void verifyRule() {
+        assertRuleProperties("OAR082 - BinaryOrByte - The string properties of the specified parameters must define a byte or binary format.", RuleType.VULNERABILITY, Severity.MAJOR, tags("vulnerability"));
+    }
+    @Override
+    public void verifyParameters() {
+        assertNumberOfParameters(1);
+        assertParameterProperties("fields-to-apply", "product,line,price", RuleParamType.STRING);
+    }
+}
