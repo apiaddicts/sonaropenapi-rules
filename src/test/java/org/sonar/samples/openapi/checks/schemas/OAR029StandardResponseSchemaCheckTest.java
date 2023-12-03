d57eb95b8cf43ab9ced651c2d5f16ef56fa1bfe5
Enrique Gomez <enrique.gomez@apiquality.io>
2023-12-03 19:36:38 +0100

    rules relisted

---
+++ src/test/java/org/sonar/samples/openapi/checks/schemas/OAR029StandardResponseSchemaCheckTest.java
@@ -0,0 +1,54 @@
+package org.sonar.samples.openapi.checks.schemas;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sonar.api.rule.Severity;
+import org.sonar.api.rules.RuleType;
+import org.sonar.api.server.rule.RuleParamType;
+import org.sonar.samples.openapi.BaseCheckTest;
+
+import apiquality.sonar.openapi.checks.schemas.OAR029StandardResponseSchemaCheck;
+
+public class OAR029StandardResponseSchemaCheckTest extends BaseCheckTest {
+
+    @Before
+    public void init() {
+        ruleName = "OAR029";
+        check = new OAR029StandardResponseSchemaCheck();
+        v2Path = getV2Path("schemas");
+        v3Path = getV3Path("schemas");
+    }
+    
+    @Test
+    public void verifyInV2() {
+        verifyV2("valid");
+    }
+
+    @Test
+    public void verifyInV2AllOf() {
+        verifyV2("valid-all-of");
+    }
+
+
+    @Test
+    public void verifyInV3() {
+        verifyV3("valid");
+    }
+
+    @Test
+    public void verifyInV3AllOf() {
+        verifyV3("valid-all-of");
+    }
+
+    @Override
+    public void verifyParameters() {
+        assertNumberOfParameters(2);
+        assertParameterProperties("response-schema", "{\"type\":\"object\",\"properties\":{\"status\":{\"type\":\"object\",\"properties\":{\"code\":{\"type\":\"integer\"},\"description\":{\"type\":\"string\"},\"internal_code\":{\"type\":\"string\"},\"errors\":{\"type\":\"array\",\"nullable\":true,\"items\":{\"type\":\"object\",\"properties\":{\"name\":{\"type\":\"string\"},\"value\":{\"type\":\"string\"}}}}},\"required\":[\"code\"]},\"payload\":{\"type\":\"any\"}},\"required\":[\"status\",\"payload\"]}", RuleParamType.STRING);
+        assertParameterProperties("path-exclusions", "/status", RuleParamType.STRING);
+    }
+
+    @Override
+    public void verifyRule() {
+        assertRuleProperties("OAR029 - StandardResponse - Response schema must be compliant with the standard", RuleType.BUG, Severity.BLOCKER, tags("schemas"));
+    }
+}
