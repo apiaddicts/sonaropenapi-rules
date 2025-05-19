b93af2212d4f75dc963dced988b84c95df84dc80
Aldo Torres <53627060+AllT2709@users.noreply.github.com>
2025-05-19 05:29:37 -0500

    feat: adding external ref openapi examples and adding it to some rules (#28)
    * feat: adding external ref openapi examples and adding it to some rules
    
    * fix: fixing openapi test
    
    * fix: fix issues in comments
    
    * fix: imports
    
    * chore: format
    
    ---------
    
    Co-authored-by: Aldo Torres <aldo.torres.pe@cloudappi.net>
    Co-authored-by: Adrian Palanques <adrian.palanques@cloudappi.net>

--- src/main/java/apiaddicts/sonar/openapi/checks/format/AbstractDefaultMediaTypeCheck.java
+++ src/main/java/apiaddicts/sonar/openapi/checks/format/AbstractDefaultMediaTypeCheck.java
@@ -20,127 +20,135 @@ import static apiaddicts.sonar.openapi.utils.JsonNodeUtils.*;
 
 public abstract class AbstractDefaultMediaTypeCheck extends BaseCheck {
 
-	private static final String DEFAULT_MEDIA_TYPE_VALUE = "application/json";
-	private static final String MEDIA_TYPE_EXCEPTIONS_VALUE = "-";
+    private static final String DEFAULT_MEDIA_TYPE_VALUE = "application/json";
+    private static final String MEDIA_TYPE_EXCEPTIONS_VALUE = "-";
 
-	private String key;
-	private String section;
-	private String message;
+    private String key;
+    private String section;
+    private String message;
+    protected JsonNode externalRefNode = null;
 
-	@RuleProperty(
-			key = "media-type-exceptions",
-			description = "Media type exceptions.",
-			defaultValue = MEDIA_TYPE_EXCEPTIONS_VALUE)
-	public String mediaTypeExceptionsStr = MEDIA_TYPE_EXCEPTIONS_VALUE;
+    @RuleProperty(
+            key = "media-type-exceptions",
+            description = "Media type exceptions.",
+            defaultValue = MEDIA_TYPE_EXCEPTIONS_VALUE)
+    public String mediaTypeExceptionsStr = MEDIA_TYPE_EXCEPTIONS_VALUE;
 
     private Set<String> mediaTypeExceptions;
 
-	@RuleProperty(
-			key = "default-media-type",
-			description = "Default media type.",
-			defaultValue = DEFAULT_MEDIA_TYPE_VALUE)
-	public String defaultMediaType = DEFAULT_MEDIA_TYPE_VALUE;
+    @RuleProperty(
+            key = "default-media-type",
+            description = "Default media type.",
+            defaultValue = DEFAULT_MEDIA_TYPE_VALUE)
+    public String defaultMediaType = DEFAULT_MEDIA_TYPE_VALUE;
 
-	private boolean globalSupportsDefaultMimeType = false;
+    private boolean globalSupportsDefaultMimeType = false;
 
-	public AbstractDefaultMediaTypeCheck(String key, String section, String message) {
-		this.key = key;
-		this.section = section;
-		this.message = translate(message);
-	}
+    public AbstractDefaultMediaTypeCheck(String key, String section, String message) {
+        this.key = key;
+        this.section = section;
+        this.message = translate(message);
+    }
 
-	@Override
-	public Set<AstNodeType> subscribedKinds() {
-		return ImmutableSet.of(OpenApi2Grammar.OPERATION, OpenApi3Grammar.OPERATION, OpenApi31Grammar.OPERATION, OpenApi3Grammar.RESPONSES, OpenApi31Grammar.RESPONSES);
-	}
+    @Override
+    public Set<AstNodeType> subscribedKinds() {
+        return ImmutableSet.of(OpenApi2Grammar.OPERATION, OpenApi3Grammar.OPERATION, OpenApi31Grammar.OPERATION, OpenApi3Grammar.RESPONSES, OpenApi31Grammar.RESPONSES);
+    }
 
-	@Override
-	protected void visitFile(JsonNode root) {
-		mediaTypeExceptions = Stream.of(mediaTypeExceptionsStr.split(","))
-				.map(String::trim)
-				.map(String::toLowerCase)
-				.collect(Collectors.toSet());
-		globalSupportsDefaultMimeType = (root.getType() instanceof OpenApi2Grammar) ? supportsDefaultMimeTypeV2(root) : false;
-	}
+    @Override
+    protected void visitFile(JsonNode root) {
+        mediaTypeExceptions = Stream.of(mediaTypeExceptionsStr.split(","))
+                .map(String::trim)
+                .map(String::toLowerCase)
+                .collect(Collectors.toSet());
+        globalSupportsDefaultMimeType = (root.getType() instanceof OpenApi2Grammar) ? supportsDefaultMimeTypeV2(root) : false;
+    }
 
-	@Override
-	public void visitNode(JsonNode node) {
-		if (node.getType() instanceof OpenApi2Grammar) {
-			visitV2Node(node);
-		} else {
-			visitV3Node(node);
-		}
-	}
+    @Override
+    public void visitNode(JsonNode node) {
+        if (node.getType() instanceof OpenApi2Grammar) {
+            visitV2Node(node);
+        } else {
+            visitV3Node(node);
+        }
+    }
 
-	private void visitV2Node(JsonNode node) {
-		JsonNode sectionNode = node.get(section);
-		boolean definesMimeTypes = !sectionNode.isMissing();
+    private void visitV2Node(JsonNode node) {
+        JsonNode sectionNode = node.get(section);
+        boolean definesMimeTypes = !sectionNode.isMissing();
 
-		if (definesMimeTypes) {
-			if (!supportsDefaultMimeTypeV2(node)) {
-				addIssue(key, message, sectionNode.key());
-			}
-		} else {
-			if (!globalSupportsDefaultMimeType) {
-				addIssue(key, message, node.key());
-			}
-		}
-	}
+        if (definesMimeTypes) {
+            if (!supportsDefaultMimeTypeV2(node)) {
+                addIssue(key, message, sectionNode.key());
+            }
+        } else {
+            if (!globalSupportsDefaultMimeType) {
+                addIssue(key, message, node.key());
+            }
+        }
+    }
 
-	private void visitV3Node(JsonNode node) {
-		if ( node.getType() == OpenApi3Grammar.OPERATION && section.equals("consumes") ) {
-			String operation = node.key().getTokenValue().toLowerCase();
-			if ( operation.equals("post") || operation.equals("put") || operation.equals("patch") ) {
-				visitContentNode(node);
-			} else {
-				JsonNode requestBodyNode = node.at("/requestBody");
-				if (!requestBodyNode.isMissing()) {
-					addIssue(key, translate("OAR010.error-request-body-not-allowed", operation.toUpperCase()), node.key());
-				}
-			}
-		}
+    private void visitV3Node(JsonNode node) {
+        if (node.getType() == OpenApi3Grammar.OPERATION && section.equals("consumes")) {
+            String operation = node.key().getTokenValue().toLowerCase();
+            if (operation.equals("post") || operation.equals("put") || operation.equals("patch")) {
+                visitContentNode(node);
+            } else {
+                JsonNode requestBodyNode = node.at("/requestBody");
+                if (!requestBodyNode.isMissing()) {
+                    addIssue(key, translate("OAR010.error-request-body-not-allowed", operation.toUpperCase()), node.key());
+                }
+            }
+        }
 
-		if (node.getType() == OpenApi3Grammar.RESPONSES && section.equals("produces")) {
+        if (node.getType() == OpenApi3Grammar.RESPONSES && section.equals("produces")) {
             List<JsonNode> responseCodes = node.properties().stream().collect(Collectors.toList());
             for (JsonNode jsonNode : responseCodes) {
                 if (!jsonNode.key().getTokenValue().equals("204")) {
-                    JsonNode responseNode = jsonNode.resolve();
-                    visitContentNode(responseNode);
+                    boolean externalRefManagement = false;
+                    if (isExternalRef(jsonNode) && externalRefNode == null) {
+                        externalRefNode = jsonNode;
+                        externalRefManagement = true;
+                    }
+                    jsonNode = resolve(jsonNode);
+                    visitContentNode(jsonNode);
+                    if (externalRefManagement) externalRefNode = null;
                 }
             }
-		}
-	}
+        }
+    }
 
-	private void visitContentNode(JsonNode node) {
-		JsonNode contentNode;
-		if (section.equals("consumes")) {
-			JsonNode requestBodyNode = node.at("/requestBody");
-			contentNode = resolve(requestBodyNode).at("/content");
-		} else {
-			contentNode = node.at("/content");
-		}
-		boolean definesMimeTypes = !contentNode.isMissing();
-		if (definesMimeTypes) {
-			if( !supportsDefaultMimeTypeV3(contentNode) ) {
-				addIssue(key, message, contentNode.key());
-			}
-		} else {
-			addIssue(key, message, node.key());
-		}
-	}
+    private void visitContentNode(JsonNode node) {
+        JsonNode contentNode;
+        if (section.equals("consumes")) {
+            JsonNode requestBodyNode = node.at("/requestBody");
+            contentNode = resolve(requestBodyNode).at("/content");
+        } else {
+            contentNode = node.at("/content");
+        }
+        boolean definesMimeTypes = !contentNode.isMissing();
+        if (definesMimeTypes) {
+            if (!supportsDefaultMimeTypeV3(contentNode)) {
+                addIssue(key, message, contentNode.key());
+            }
+        } else {
+            addIssue(key, message, node.key());
+        }
+    }
 
-	private boolean supportsDefaultMimeTypeV2(JsonNode node) {
-		JsonNode consumes = node.get(section);
-		if (consumes.isMissing() || consumes.isNull()) return false;
-		List<JsonNode> mimeTypeNodes = consumes.elements();
-		if (mimeTypeNodes.stream().map(AstNode::getTokenValue).anyMatch(mediaTypeExceptions::contains)) return true;
-		return mimeTypeNodes.stream().map(AstNode::getTokenValue).anyMatch(defaultMediaType::equals);
-	}
+    private boolean supportsDefaultMimeTypeV2(JsonNode node) {
+        JsonNode consumes = node.get(section);
+        if (consumes.isMissing() || consumes.isNull()) return false;
+        List<JsonNode> mimeTypeNodes = consumes.elements();
+        if (mimeTypeNodes.stream().map(AstNode::getTokenValue).anyMatch(mediaTypeExceptions::contains)) return true;
+        return mimeTypeNodes.stream().map(AstNode::getTokenValue).anyMatch(defaultMediaType::equals);
+    }
 
-	private boolean supportsDefaultMimeTypeV3(JsonNode content) {
-		if (content.isMissing() || content.isNull()) return false;
-		Map<String, JsonNode> properties = content.propertyMap();
-		if (properties.entrySet().stream().map(entry -> entry.getKey()).anyMatch(mediaTypeExceptions::contains)) return true;
-		return properties.entrySet().stream().map(entry -> entry.getKey()).anyMatch(defaultMediaType::equals);
-	}
+    private boolean supportsDefaultMimeTypeV3(JsonNode content) {
+        if (content.isMissing() || content.isNull()) return false;
+        Map<String, JsonNode> properties = content.propertyMap();
+        if (properties.entrySet().stream().map(entry -> entry.getKey()).anyMatch(mediaTypeExceptions::contains))
+            return true;
+        return properties.entrySet().stream().map(entry -> entry.getKey()).anyMatch(defaultMediaType::equals);
+    }
 }
