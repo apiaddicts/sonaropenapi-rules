5232f906798cc40e66856e9c35c7c8daa9113409
Melsy Huamani <melsyhuamanivargas@gmail.com>
2026-02-16 00:28:31 -0500

    refactor: unify schema naming convention checks oar067, oar068, oar069

---
+++ src/main/java/apiaddicts/sonar/openapi/checks/format/AbstractSchemaNamingConventionCheck.java
@@ -0,0 +1,151 @@
+package apiaddicts.sonar.openapi.checks.format;
+
+import apiaddicts.sonar.openapi.utils.ExternalRefHandler;
+import com.google.common.collect.ImmutableSet;
+import com.sonar.sslr.api.AstNodeType;
+
+import java.util.Map;
+import java.util.Set;
+import org.apiaddicts.apitools.dosonarapi.api.v2.OpenApi2Grammar;
+import org.apiaddicts.apitools.dosonarapi.api.v3.OpenApi3Grammar;
+import org.apiaddicts.apitools.dosonarapi.api.v31.OpenApi31Grammar;
+import org.apiaddicts.apitools.dosonarapi.sslr.yaml.grammar.JsonNode;
+import static apiaddicts.sonar.openapi.utils.JsonNodeUtils.isOperation;
+
+public abstract class AbstractSchemaNamingConventionCheck extends AbstractNamingConventionCheck {
+
+  protected static final String SCHEMA = "schema";
+  protected static final String PROPERTIES = "properties";
+
+  private final ExternalRefHandler handleExternalRef = new ExternalRefHandler();
+
+  protected AbstractSchemaNamingConventionCheck(String key, String message, String namingConvention) {
+    super(key, message, namingConvention);
+  }
+
+  @Override
+  public Set<AstNodeType> subscribedKinds() {
+    return ImmutableSet.of(OpenApi2Grammar.PATHS, OpenApi3Grammar.PATHS, OpenApi31Grammar.PATHS, OpenApi2Grammar.PATH,
+        OpenApi3Grammar.PATH, OpenApi31Grammar.PATH);
+  }
+
+  @Override
+  public void visitNode(JsonNode node) {
+    if (OpenApi2Grammar.PATHS.equals(node.getType()) || OpenApi3Grammar.PATHS.equals(node.getType())) {
+      visitPathsNode(node);
+    }
+    if (OpenApi2Grammar.PATH.equals(node.getType()) || OpenApi3Grammar.PATH.equals(node.getType())) {
+      visitPathNode(node);
+    }
+  }
+
+  private void visitPathsNode(JsonNode pathsNode) {
+    pathsNode.propertyMap().values().forEach(pathNode -> pathNode.propertyMap().values().forEach(operationNode -> {
+      JsonNode parameters = operationNode.get("parameters");
+      if (!parameters.isMissing()) {
+        parameters.elements().stream()
+            .filter(p -> "body".equals(p.get("in").getTokenValue()))
+            .forEach(this::visitParameterNode);
+      }
+      handleExternalRef.resolve(operationNode.get("requestBody"), this::visitRequestBodyNode);
+      handleExternalRef.resolve(operationNode.get("responses"), this::visitResponsesNode);
+    }));
+  }
+
+  private void visitParameterNode(JsonNode parameterNode) {
+    JsonNode inNode = parameterNode.get("in");
+    if (!inNode.isMissing() && "body".equals(inNode.getTokenValue())) {
+      JsonNode schemaNode = parameterNode.get(SCHEMA);
+      if (!schemaNode.isMissing()) {
+        visitSchemaNode(schemaNode);
+      }
+    } else {
+      JsonNode nameNode = parameterNode.get("name");
+      if (!nameNode.isMissing()) {
+        String name = nameNode.getTokenValue();
+        validateNamingConvention(name, nameNode);
+      }
+    }
+  }
+
+  private void visitRequestBodyNode(JsonNode requestBodyNode) {
+    JsonNode contentNode = requestBodyNode.get("content");
+    if (!contentNode.isMissing()) {
+      for (JsonNode mediaTypeNode : contentNode.propertyMap().values()) {
+        JsonNode schemaNode = mediaTypeNode.get(SCHEMA);
+        if (!schemaNode.isMissing()) {
+          visitSchemaNode(schemaNode);
+        }
+      }
+    }
+  }
+
+  private void visitResponsesNode(JsonNode responsesNode) {
+    for (JsonNode responseNode : responsesNode.propertyMap().values()) {
+      JsonNode contentNode = responseNode.get("content");
+      if (!contentNode.isMissing()) {
+        for (JsonNode mediaTypeNode : contentNode.propertyMap().values()) {
+          JsonNode schemaNode = mediaTypeNode.get(SCHEMA);
+          if (!schemaNode.isMissing()) {
+            visitSchemaNode(schemaNode);
+          }
+        }
+      } else {
+        JsonNode schemaNode = responseNode.get(SCHEMA);
+        if (!schemaNode.isMissing()) {
+          visitSchemaNode(schemaNode);
+        }
+      }
+    }
+  }
+
+  private void visitSchemaNode(JsonNode schemaNode) {
+    Map<String, JsonNode> properties = schemaNode.propertyMap();
+    if (properties.containsKey(PROPERTIES)) {
+      Map<String, JsonNode> schemaProperties = schemaNode.get(PROPERTIES).propertyMap();
+      for (JsonNode property : schemaProperties.values()) {
+        JsonNode nameNode = property.key();
+        String name = nameNode.getTokenValue();
+        validateNamingConvention(name, nameNode);
+      }
+    }
+  }
+
+  private void visitPathNode(JsonNode node) {
+    node.properties().stream()
+        .filter(prop -> isOperation(prop))
+        .map(JsonNode::value)
+        .map(operation -> operation.get("responses"))
+        .filter(responses -> !responses.isMissing())
+        .flatMap(responses -> responses.propertyMap().values().stream())
+        .forEach(response -> handleExternalRef.resolve(response, resolved -> {
+          if (resolved.getType().equals(OpenApi2Grammar.RESPONSE)) {
+            visitSchemaNode2(resolved);
+          } else if (resolved.getType().equals(OpenApi3Grammar.RESPONSE)) {
+            JsonNode content = resolved.at("/content");
+            if (!content.isMissing()) {
+              content.propertyMap().forEach((mediaType, mediaTypeNode) -> {
+                if (mediaType.toLowerCase().contains("json")) {
+                  visitSchemaNode2(mediaTypeNode);
+                }
+              });
+            }
+          }
+        }));
+  }
+
+  private void visitSchemaNode2(JsonNode responseOrMediaTypeNode) {
+    JsonNode schemaNode = responseOrMediaTypeNode.get(SCHEMA);
+    if (schemaNode.isMissing()) {
+      schemaNode = responseOrMediaTypeNode.value().get(SCHEMA);
+    }
+
+    handleExternalRef.resolve(schemaNode, schemaResolved -> {
+      JsonNode propsNode = schemaResolved.get(PROPERTIES);
+      if (!propsNode.isMissing() && propsNode.isObject()) {
+        propsNode.propertyMap()
+            .forEach((name, property) -> validateNamingConvention(name, handleExternalRef.getTrueNode(property.key())));
+      }
+    });
+  }
+}
--- src/main/java/apiaddicts/sonar/openapi/checks/format/OAR067CamelCaseNamingConventionCheck.java
+++ src/main/java/apiaddicts/sonar/openapi/checks/format/OAR067CamelCaseNamingConventionCheck.java
@@ -1,114 +1,14 @@
 package apiaddicts.sonar.openapi.checks.format;
 
-import com.google.common.collect.ImmutableSet;
-import com.sonar.sslr.api.AstNodeType;
 import org.sonar.check.Rule;
-import org.apiaddicts.apitools.dosonarapi.api.v2.OpenApi2Grammar;
-import org.apiaddicts.apitools.dosonarapi.api.v3.OpenApi3Grammar;
-import org.apiaddicts.apitools.dosonarapi.api.v31.OpenApi31Grammar;
-import org.apiaddicts.apitools.dosonarapi.sslr.yaml.grammar.JsonNode;
-import apiaddicts.sonar.openapi.utils.ExternalRefHandler;
-
-import java.util.Map;
-import java.util.Set;
 
 @Rule(key = OAR067CamelCaseNamingConventionCheck.KEY)
-public class OAR067CamelCaseNamingConventionCheck extends AbstractNamingConventionCheck {
+public class OAR067CamelCaseNamingConventionCheck extends AbstractSchemaNamingConventionCheck {
 
     public static final String KEY = "OAR067";
     private static final String MESSAGE = "OAR067.error";
-    private static final String NAMING_CONVENTION = CAMEL_CASE;
-    private static final String SCHEMA = "schema";
-
-    private final ExternalRefHandler handleExternalRef = new ExternalRefHandler();
 
     public OAR067CamelCaseNamingConventionCheck() {
-        super(KEY, MESSAGE, NAMING_CONVENTION);
-    }
-
-    @Override
-    public Set<AstNodeType> subscribedKinds() {
-        return ImmutableSet.of(OpenApi2Grammar.PATHS, OpenApi3Grammar.PATHS, OpenApi31Grammar.PATHS);
-    }
-
-    @Override
-    public void visitNode(JsonNode node) {
-        if (OpenApi2Grammar.PATHS.equals(node.getType()) || OpenApi3Grammar.PATHS.equals(node.getType()) || OpenApi31Grammar.PATHS.equals(node.getType())) {
-            visitPathsNode(node);
-        }
-    }
-
-    private void visitPathsNode(JsonNode pathsNode) {
-        pathsNode.propertyMap().values().forEach(pathNode ->
-            pathNode.propertyMap().values().forEach(operationNode -> {
-                JsonNode parameters = operationNode.get("parameters");
-                if (!parameters.isMissing()) {
-                    parameters.elements().stream()
-                            .filter(p -> "body".equals(p.get("in").getTokenValue()))
-                            .forEach(this::visitParameterNode);
-                }
-                handleExternalRef.resolve(operationNode.get("requestBody"), this::visitRequestBodyNode);
-                handleExternalRef.resolve(operationNode.get("responses"), this::visitResponsesNode);
-            })
-        );
-    }
-
-    private void visitParameterNode(JsonNode parameterNode) {
-        JsonNode inNode = parameterNode.get("in");
-        if (!inNode.isMissing() && "body".equals(inNode.getTokenValue())) {
-            JsonNode schemaNode = parameterNode.get(SCHEMA);
-            if (!schemaNode.isMissing()) {
-                visitSchemaNode(schemaNode);
-            }
-        } else {
-            JsonNode nameNode = parameterNode.get("name");
-            if (!nameNode.isMissing()) {
-                String name = nameNode.getTokenValue();
-                validateNamingConvention(name, nameNode);
-            }
-        }
-    }
-
-    private void visitRequestBodyNode(JsonNode requestBodyNode) {
-        JsonNode contentNode = requestBodyNode.get("content");
-        if (!contentNode.isMissing()) {
-            for (JsonNode mediaTypeNode : contentNode.propertyMap().values()) {
-                JsonNode schemaNode = mediaTypeNode.get(SCHEMA);
-                if (!schemaNode.isMissing()) {
-                    visitSchemaNode(schemaNode);
-                }
-            }
-        }
-    }
-
-    private void visitResponsesNode(JsonNode responsesNode) {
-        for (JsonNode responseNode : responsesNode.propertyMap().values()) {
-            JsonNode contentNode = responseNode.get("content");
-            if (!contentNode.isMissing()) {
-                for (JsonNode mediaTypeNode : contentNode.propertyMap().values()) {
-                    JsonNode schemaNode = mediaTypeNode.get(SCHEMA);
-                    if (!schemaNode.isMissing()) {
-                        visitSchemaNode(schemaNode);
-                    }
-                }
-            } else {
-                JsonNode schemaNode = responseNode.get(SCHEMA);
-                if (!schemaNode.isMissing()) {
-                    visitSchemaNode(schemaNode);
-                }
-            }
-        }
-    }
-
-    private void visitSchemaNode(JsonNode schemaNode) {
-        Map<String, JsonNode> properties = schemaNode.propertyMap();
-        if (properties.containsKey("properties")) {
-            Map<String, JsonNode> schemaProperties = schemaNode.get("properties").propertyMap();
-            for (JsonNode property : schemaProperties.values()) {
-                JsonNode nameNode = property.key();
-                String name = nameNode.getTokenValue();
-                validateNamingConvention(name, nameNode);
-            }
-        }
+        super(KEY, MESSAGE, CAMEL_CASE);
     }
 }
